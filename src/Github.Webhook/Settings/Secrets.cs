b51537f4aea88628347af4612a954f9925a6f784
wellingtonpoll <wellingtonpoleti@gmail.com>
2023-09-21 20:24:52 -0300

    [feat] change secret name

--- src/Github.Webhook/Settings/Secrets.cs
+++ src/Github.Webhook/Settings/Secrets.cs
@@ -5,10 +5,13 @@
     /// </summary>
     public static class Secrets
     {
+        /// <summary>
+        /// Github webhook secret key
+        /// </summary>
         public static string ApiGithubSecretKey = "API_GITHUB_SECRET";
 
         /// <summary>
-        /// Github webhook secret
+        /// Github webhook secret value
         /// </summary>
         public static string ApiGithubSecret { get; set; } = string.Empty;
     }
